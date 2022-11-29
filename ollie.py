2fc58af534850f780f43338ee72cb711572dd8c0
Anirudh Dilli <anirudhdilli@gmail.com>
2022-11-29 07:50:00 +0530

    did something

--- ollie.py
+++ ollie.py
@@ -1,6 +1,8 @@
-import os
-import sys
 import fileinput
+import os
+from sys import argv
+
+#########################################################
 
 print('''
  .----------------. .----------------. .----------------. .----------------. .----------------. 
@@ -14,39 +16,40 @@ print('''
 | |              | | |              | | |              | | |              | | |              | |
 | '--------------' | '--------------' | '--------------' | '--------------' | '--------------' |
  '----------------' '----------------' '----------------' '----------------' '----------------' 
-                                                                                    @0day, @VainXploits
+                                                                                    @0dayCTF, @VainXploits
                                                                                       Twitter: https://twitter.com/0dayCTF
                                                                                       Twitter: https://twitter.com/VainXploits
                                                                                     
 ''')
+#######################NOTES-STR#######################
+# IP_var = input("Receiving IP: ")
+# PORT_var = input("Receiving PORT: ")
+# IP = print(open('file.txt', 'r').read().replace('IP', IP_var) ) # ----> Really Really important.
+# print(open('file.txt', 'r').read().replace(IP + 'PORT', PORT_var))
+#######################NOTES-END#######################
 
-IP_var = input("Receiving IP: ")
-PORT_var = input("Receiving PORT: ")
+def display_payload_options():
+  f = open('demo.txt', 'r')
+  file_contents = f.read()
+  print(file_contents)
+  f.close()
 
+def yet_to_decide():
+  # open the sample file used
+  file = open('assets/payloads.txt')
+  # read the content of the file opened
+  content = file.readlines()
+  # read 10th line from the file
+  print(content[0])
 
-IP = print(open('file.txt', 'r').read().replace('IP', IP_var) ) # ----> Really Really important.
-print(open('file.txt', 'r').read().replace(IP + 'PORT', PORT_var))
+#yet_to_decide()
 
-'''file = open('file.txt')
-content = file.readlines()
+display_payload_options()
 
-originalStdOut = sys.stdout
-with open('demo.txt', 'w') as f:
-    sys.stdout = f 
-    print(content[5])
-    # Reset the standard output
-    sys.stdout = originalStdOut'''
-
-
-
-
-
-'''# open the sample file used
-file = open('test.txt')
-  
-# read the content of the file opened
-content = file.readlines()
-  
-# read 10th line from the file
-print("tenth line")
-print(content[9]) >'''
+# TODO: Work on printing the stuff from display_payload_options() function in a grid format. Ref: https://stackoverflow.com/questions/32460832/print-a-list-of-strings-in-a-grid-format-python
+# TODO: Decide on a name for the yet_to_decide() function.
+# TODO: Do the manual labour of putting all the payloads into a txt file.
+# TODO: Work on functions for encoding options & shell options.
+# TODO: Work on listener options.
+# TODO: Work on how I can do most of this from single command in the terminal instead of throwing the user a whole bunch of fucking prompts once they run the tool.
+# TODO: ^^ Includes how to select Reverse, Bind or MSF and how to select a particular payload they want(inside the sections in the revhsells website).
