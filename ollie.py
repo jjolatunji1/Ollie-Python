fc2851e127e29f2cbb97accc7f2dca0e45d778d5
Anirudh Dilli <anirudhdilli@gmail.com>
2022-11-29 20:56:59 +0530

    worked on single urlencoding

--- ollie.py
+++ ollie.py
@@ -1,6 +1,7 @@
 import fileinput
 import os
 from sys import argv
+import urllib.parse
 
 #########################################################
 
@@ -39,17 +40,22 @@ def yet_to_decide():
   file = open('assets/payloads.txt')
   # read the content of the file opened
   content = file.readlines()
-  # read 10th line from the file
+  # read nth line from the file
   print(content[0])
 
 #yet_to_decide()
 
-display_payload_options()
+# display_payload_options()
+
+# This function is for urlencoding the payloads.
+def url_encode():
+  echo = urllib.parse.quote("String I want in here(the shells that want to be URL encoded by the user.)", safe='')
+  print(echo)
 
 # TODO: Work on printing the stuff from display_payload_options() function in a grid format. Ref: https://stackoverflow.com/questions/32460832/print-a-list-of-strings-in-a-grid-format-python
 # TODO: Decide on a name for the yet_to_decide() function.
 # TODO: Do the manual labour of putting all the payloads into a txt file.
-# TODO: Work on functions for encoding options & shell options.
+# TODO: Work on functions for encoding options & shell options. UPDATE 1: DONE WITH SINGLE URL ENCODING
 # TODO: Work on listener options.
 # TODO: Work on how I can do most of this from single command in the terminal instead of throwing the user a whole bunch of fucking prompts once they run the tool.
 # TODO: ^^ Includes how to select Reverse, Bind or MSF and how to select a particular payload they want(inside the sections in the revhsells website).
